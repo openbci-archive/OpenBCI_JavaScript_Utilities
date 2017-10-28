1d16d06849787cdd2ec3fb39b5aafa58cd18a4c4
AJ Keller <pushtheworldllc@gmail.com>
2017-10-28 15:45:29 -0400

    ADD: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const library = 'OpenBCIUtilities';
+const libraryExport = 'default';
+
+const config = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devtool: 'eval',
+  devServer: {
+    compress: true,
+    port: 9000
+  }
+};
+
+const exportLibraryTarget = libraryTarget =>
+  Object.assign({}, config, {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `${library.toLowerCase()}.${libraryTarget}.js`,
+      library,
+      libraryTarget,
+      libraryExport
+    }
+  });
+
+module.exports = [
+  exportLibraryTarget('umd'),
+  exportLibraryTarget('var'),
+];
